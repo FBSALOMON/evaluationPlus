f5ca924f6b6817481c9bec614eb65f1e1d84d4fc
eleves <you@yourdomain.com>
2017-11-30 12:04:49 -0500

    version 1

---
+++ Evaluation+/GradeController.swift
@@ -0,0 +1,109 @@
+//=================================
+import UIKit
+//=================================
+class GradeController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //----------------
+    @IBOutlet weak var student_name_label: UILabel!
+    @IBOutlet weak var course_field: UITextField!
+    @IBOutlet weak var grade_field: UITextField!
+    @IBOutlet weak var course_grade_tableview: UITableView!
+    
+    //---------------
+    typealias studentName = String
+    typealias course = String
+    typealias grade = Double
+    //----------------
+    let userDefaultsObj = UserDefaultManager()
+    var studentGrades: [studentName: [course: grade]]!
+    var arrOfCourses: [course] = []
+    var arrOfGrades: [grade] = []
+    //----------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadUserDefaults()
+        let name = userDefaultsObj.getValue(theKey: "name") as! String
+        student_name_label.text = name
+        fillUpArray()
+        loadCourseAndGrade()
+    }
+    //----------------
+    //virifier si il ya des valeur au pas
+    func loadUserDefaults() {
+        if userDefaultsObj.doesKeyExist(theKey: "grades") {
+            studentGrades = userDefaultsObj.getValue(theKey: "grades") as! [studentName: [course: grade]]
+        } else {
+            studentGrades = [studentName: [course: grade]]() //une valeur assigner mais vide
+        }
+    }
+    //----------------
+    func fillUpArray() {
+        let name = student_name_label.text
+        let course_and_grade = studentGrades[name!]
+        arrOfCourses = [course](course_and_grade!.keys)
+        arrOfGrades = [grade](course_and_grade!.values)
+    }
+    //----------------
+    func loadCourseAndGrade() {
+        let name = student_name_label.text
+        var course_and_grade = studentGrades[name!]
+        if course_and_grade?.keys.count == 0 {
+            studentGrades[name!] = ["Example": 10]
+            course_and_grade = studentGrades[name!]
+        }
+        let the_course = [course](course_and_grade!.keys)[0]
+        let the_grade = [grade](course_and_grade!.values)[0]
+        course_field.text = the_course
+        grade_field.text = String(the_grade)
+        course_grade_tableview.reloadData()
+    }
+    //----------------
+    @IBAction func save_course_and_grade(_ sender: UIButton) {
+        //let value = [course_field.text!: Double(grade_field.text!)!]
+        let name = student_name_label.text
+        var student_courses = studentGrades[name!]
+        student_courses![course_field.text!] = Double(grade_field.text!)
+        studentGrades[name!] = student_courses
+        userDefaultsObj.setKey(theValue: studentGrades as AnyObject, theKey: "grades")
+        fillUpArray()
+        course_grade_tableview.reloadData()
+        print(studentGrades)
+    }
+    //----------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrOfCourses.count
+    }
+    //----------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = arrOfCourses[indexPath.row]
+        return cell
+    }
+    //----------------
+    //pour supprimer
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            let name = student_name_label.text
+            var student_courses1 = studentGrades[name!]
+            student_courses1 = [:]
+            arrOfCourses.remove(at: indexPath.row)
+            arrOfGrades.remove(at: indexPath.row)
+            
+            if (arrOfCourses.count == 0) {
+                student_courses1 = [:]
+            } else {
+                for a in 0...(arrOfCourses.count - 1) {
+                student_courses1![arrOfCourses[a]] = Double(arrOfGrades[a])
+                }
+            }
+            studentGrades[name!] = student_courses1
+        
+            userDefaultsObj.setKey(theValue: studentGrades as AnyObject, theKey: "grades")
+            
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+}
+//=================================
+
+
+
--- Evaluation+/ViewController.swift
+++ Evaluation+/ViewController.swift
@@ -1,25 +1,75 @@
-//
-//  ViewController.swift
-//  Evaluation+
-//
-//  Created by eleves on 17-11-23.
-//  Copyright © 2017 eleves. All rights reserved.
-//
-
+//============================
 import UIKit
-
-class ViewController: UIViewController {
-
+//============================
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    //---------------
+    @IBOutlet weak var student_name_feild: UITextField!
+    @IBOutlet weak var student_name_tableview: UITableView!
+    //---------------
+    typealias studentName = String
+    typealias course = String
+    typealias grade = Double
+    //---------------
+    let userDefaultsObj = UserDefaultManager()
+    var studentGrades: [studentName: [course: grade]]!
+    //---------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadUserDefaults()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //---------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentGrades.count
     }
-
-
+    //---------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = [studentName](studentGrades.keys)[indexPath.row]
+        return cell
+    }
+    //---------------
+    //la méthode qui devient active lorsque quand click sur une cellule
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let name = [studentName](studentGrades.keys)[indexPath.row] //chercher le nom de l acell que je vé cicker de su
+        userDefaultsObj.setKey(theValue: name as AnyObject, theKey: "name") //j ai en memoir le nom de l'eleve ke j ai clicker ,sous la clé name
+        performSegue(withIdentifier: "seg", sender: nil) //partir de segue
+    }
+    //---------------
+    //pour supprimer
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            let name = [studentName](studentGrades.keys)[indexPath.row]
+            studentGrades[name] = nil
+            userDefaultsObj.setKey(theValue: studentGrades as AnyObject, theKey: "grades")
+            
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    //---------------
+    //func pour return le clavier
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    //---------------
+    //virifier si il ya des valeur au pas
+    func loadUserDefaults() {
+        if userDefaultsObj.doesKeyExist(theKey: "grades") {
+            studentGrades = userDefaultsObj.getValue(theKey: "grades") as! [studentName: [course: grade]]
+        } else {
+            studentGrades = [studentName: [course: grade]]() //une valeur assigner mais vide
+        }
+    }
+    //---------------
+    @IBAction func addStudent(_ sender: UIButton) {
+        if student_name_feild.text != "" {
+            //il faut ajouter un alert que dit le champs est vide
+            studentGrades[student_name_feild.text!] = ["EMPTY": 0.0]//quand je creer mon etudent au debut j pas de cours et j pa de note
+            student_name_feild.text = "" //vider le champ
+            userDefaultsObj.setKey(theValue: studentGrades as AnyObject, theKey: "grades") //garder l'information
+            student_name_tableview.reloadData() //faire la mise a jour de mon tableview
+        }
+    }
+    //---------------
 }
-
+//============================
